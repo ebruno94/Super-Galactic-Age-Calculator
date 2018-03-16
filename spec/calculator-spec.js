4a6100e986ae6e77baf689fb6822c9d9c17a6c5c
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-03-16 08:36:13 -0700

    Add readme.md with specs, and add second test for function that turns age (in years )to seconds

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,7 +2,11 @@ import {Calculator} from "./../src/js/calculator.js";
 
 describe('Calculator', function() {
   it('should test that Calculator stores age', function(){
-      let newCalculator = new Calculator(15);
-      expect(newCalculator.age).toEqual(15);
+    let newCalculator = new Calculator(15);
+    expect(newCalculator.age).toEqual(15);
   });
+  it('should test that given age in years will be turned to seconds', function(){
+    let newCalculator = new Calculator(1);
+    expect(newCalculator.ageSeconds()).toEqual(315360000);  
+  })
 });

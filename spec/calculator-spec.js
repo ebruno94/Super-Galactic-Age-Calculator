3588c4cc27ea39a3d6121f6f5773d7dd68400931
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-03-16 08:39:37 -0700

    Complete function, pass test 2

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -7,6 +7,6 @@ describe('Calculator', function() {
   });
   it('should test that given age in years will be turned to seconds', function(){
     let newCalculator = new Calculator(1);
-    expect(newCalculator.ageSeconds()).toEqual(315360000);  
+    expect(newCalculator.ageSeconds()).toEqual(31536000);  
   })
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -2,4 +2,8 @@ export class Calculator{
     constructor(age){
         this.age = age;
     }
+
+    ageSeconds(){
+        return this.age * 365 * 24 * 60 * 60;
+    }
 }

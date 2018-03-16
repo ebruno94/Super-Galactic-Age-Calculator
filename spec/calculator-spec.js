8629c880809379874ed3b258373eead43b7f3bf7
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-03-16 09:28:37 -0700

    Change structure of Calculator class, tests still pass

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,28 +1,32 @@
 import {Calculator} from "./../src/js/calculator.js";
 
 describe('Calculator', function() {
-  it('should test that Calculator stores age', function(){
-    let newCalculator = new Calculator(15);
-    expect(newCalculator.age).toEqual(15);
-  });
+  // it('should test that Calculator stores age', function(){
+  //   let newCalculator = new Calculator(15);
+  //   expect(newCalculator.age).toEqual(15);
+  // });
   it('should test that given age in years will be turned to seconds', function(){
-    let newCalculator = new Calculator(1);
-    expect(newCalculator.ageSeconds()).toEqual(31536000);
+    let newCalculator = new Calculator();
+    expect(newCalculator.ageSeconds(1)).toEqual(31536000);
   });
   it('should return correct conversion of Earth age to Mercury Age', function(){
-     let newCalculator = new Calculator(12);
-     expect(newCalculator.ageMercury()).toEqual(50.00);
+     let newCalculator = new Calculator();
+     expect(newCalculator.ageMercury(12)).toEqual(50.00);
   });
   it('should return correct conversion of Earth age to Venus age', function(){
-      let newCalculator = new Calculator(12);
-      expect(newCalculator.ageVenus()).toEqual(19.35);
+      let newCalculator = new Calculator();
+      expect(newCalculator.ageVenus(12)).toEqual(19.35);
   })
   it('should return correct conversion of Earth age to Mars', function(){
-      let newCalculator = new Calculator(12);
-      expect(newCalculator.ageMars()).toEqual(6.38);
+      let newCalculator = new Calculator();
+      expect(newCalculator.ageMars(12)).toEqual(6.38);
   });
   it('should return correct conversion of Earth age to Jupiter age', function(){
-      let newCalculator = new Calculator(12);
-      expect(newCalculator.ageJupiter()).toEqual(1.01);
+      let newCalculator = new Calculator();
+      expect(newCalculator.ageJupiter(12)).toEqual(1.01);
+  });
+  it('should return number of years a user has left to live on given planet', function(){
+      let newCalculator = new Calculator();
+      expect(newCalculator.ageJupiter(12)).toEqual(1.01);
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,25 +1,26 @@
 export class Calculator{
-    constructor(age){
-        this.age = age;
+    // constructor(age){
+    //     this.age = age;
+    // }
+
+    ageSeconds(age){
+        return age * 365 * 24 * 60 * 60;
     }
 
-    ageSeconds(){
-        return this.age * 365 * 24 * 60 * 60;
+    ageMercury(age){
+        return Math.round((age / 0.24) * 100) / 100 ;
     }
 
-    ageMercury(){
-        return Math.round((this.age / 0.24) * 100) / 100 ;
+    ageVenus(age){
+        return Math.round((age / 0.62) * 100) / 100;
     }
 
-    ageVenus(){
-        return Math.round((this.age / 0.62) * 100) / 100;
+    ageMars(age){
+        return Math.round((age / 1.88) * 100) / 100;
     }
 
-    ageMars(){
-        return Math.round((this.age / 1.88) * 100) / 100;
+    ageJupiter(age){
+        return Math.round((age / 11.86) * 100) / 100;
     }
 
-    ageJupiter(){
-        return Math.round((this.age / 11.86) * 100) / 100;
-    }
 }
